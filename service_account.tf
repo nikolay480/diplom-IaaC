6c6ed3e4fcd59e2b9b9b8145674158434e946a55
Nikolay Chaykovskiy <nickrer1988@gmail.com>
2023-10-10 23:23:11 +0500

    add  cr-pusher, puller roles for SA

--- service_account.tf
+++ service_account.tf
@@ -6,3 +6,16 @@ data "yandex_iam_service_account" "sa" {
  # description = "Service account for manage instances"
 }
 
+resource "yandex_resourcemanager_folder_iam_member" "sa-cr-pusher" {
+  folder_id = var.folder_id
+  role      = "container-registry.images.pusher"
+  member    = "serviceAccount:${data.yandex_iam_service_account.sa.id}"
+
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa-cr-puller" {
+  folder_id = var.folder_id
+  role      = "container-registry.images.puller"
+  member    = "serviceAccount:${data.yandex_iam_service_account.sa.id}"
+  
+}
