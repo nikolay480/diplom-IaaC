4cba5e11e7bd46bca563f829030692058e2d996a
Administrator <admin@example.com>
2023-10-17 16:41:01 +0500

    delete comment

--- instance_group.tf
+++ instance_group.tf
@@ -112,39 +112,4 @@ resource "yandex_compute_instance_group" "worker-nodes" {
     max_unavailable = 1
     max_expansion   = 0
   }
-
-  # load_balancer {
-  #   target_group_name        = "target-group"
-  #   target_group_description = "load balancer target group"
-  # }
 }
-
-#================== -LOAD BALANCER- =========================
-
-# resource "yandex_lb_network_load_balancer" "lb-1" {
-#   name = "network-load-balancer-1"
-
-#   listener {
-#     name = "network-load-balancer-1-listener"
-#     port = 80
-#     external_address_spec {
-#       ip_version = "ipv4"
-#       address = "158.160.55.118"
-#     }
-#   }
-
-#   attached_target_group {
-#     target_group_id = yandex_compute_instance_group.worker-nodes.load_balancer.0.target_group_id
-
-#     healthcheck {
-#       name = "http"
-#       http_options {
-#         port = 80
-#         path = "/index.html"
-#       }
-#     }
-#   }
-#   depends_on = [
-#     yandex_compute_instance_group.worker-nodes
-#   ]
-# }
